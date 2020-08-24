eec9a18128ff60e3657602da99d94fe30a60e961
Andres Rodriguez <andresrodriguez4@outlook.com.ar>
2020-08-24 04:31:03 -0300

    milestone 3 finished

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import 'normalize.css';
 import './App.css';
 import Display from './components/Display';
 import ButtonPanel from './components/ButtonPanel';
 
 function App() {
   return (
-    <div>
+    <div className="app">
       <Display result="0" />
-      <ButtonPanel />
+      <ButtonPanel className="panel" />
     </div>
   );
 }
