19933f5f91c052b08cc6de5d32bed416c0177003
Andres Rodriguez <andresrodriguez4@outlook.com.ar>
2020-08-24 22:52:28 -0300

    finished milestone 5 using context

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import './button.css';
+import { LogicContext } from './App';
 
 export default function Button(props) {
+  const { handleClick } = useContext(LogicContext);
+
   const { name, color, wide } = props;
   return (
-    <button type="button" className={`btn ${color} ${wide ? 'wide' : null}`}>
+    <button
+      type="button"
+      className={`btn ${color} ${wide ? 'wide' : null}`}
+      onClick={() => handleClick(name)}
+    >
       { name }
     </button>
   );
